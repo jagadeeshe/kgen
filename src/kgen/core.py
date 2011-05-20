d3ee568541a3bdaf413c9ae7a5e3179093480f10
Jagadeesh Babu <jagadeeshbabue@gmail.com>
2011-05-20 21:50:53 +0200

    added pattern element

--- src/kgen/core.py
+++ src/kgen/core.py
@@ -34,3 +34,39 @@ tokens = (
         'REG_REPEAT',
         'UNDER',
     )
+
+class PatternElement(list):
+    DEFAULT = 0
+    COMMIT = 1
+    REPEAT = 2
+    ALTERNATIVE = 4
+
+    def __init__(self, lex, sur, flag=0):
+        list.__init__(self, [lex, sur, flag])
+
+    def mark_COMMIT(self):
+        self[2] |= PatternElement.COMMIT
+
+    def mark_REPEAT(self):
+        self[2] |= PatternElement.REPEAT
+
+    def mark_ALTERNATIVE(self):
+        self[2] |= PatternElement.ALTERNATIVE
+
+    def isCOMMIT(self):
+        if (self[2] & PatternElement.COMMIT) == 0:
+            return False
+        else:
+            return True
+
+    def isREPEAT(self):
+        if (self[2] & PatternElement.REPEAT) == 0:
+            return False
+        else:
+            return True
+
+    def isALTERNATIVE(self):
+        if (self[2] & PatternElement.ALTERNATIVE) == 0:
+            return False
+        else:
+            return True
---
+++ test/test_kgen/test_PatternElement.py
@@ -0,0 +1,50 @@
+'''
+Created on May 20, 2011
+
+@author: jagadeeshe
+'''
+import unittest
+from kgen.core import PatternElement
+
+class PatternElementTest(unittest.TestCase):
+
+    def test_default(self):
+        p = PatternElement('a', 'b')
+        self.assertEqual('a', p[0])
+        self.assertEqual('b', p[1])
+        self.assertFalse(p.isCOMMIT())
+        self.assertFalse(p.isREPEAT())
+        self.assertFalse(p.isALTERNATIVE())
+
+    def test_COMMIT(self):
+        p = PatternElement('a', 'b')
+        p.mark_COMMIT()
+        self.assertTrue(p.isCOMMIT())
+        self.assertFalse(p.isREPEAT())
+        self.assertFalse(p.isALTERNATIVE())
+
+    def test_REPEAT(self):
+        p = PatternElement('a', 'b')
+        p.mark_REPEAT()
+        self.assertFalse(p.isCOMMIT())
+        self.assertTrue(p.isREPEAT())
+        self.assertFalse(p.isALTERNATIVE())
+
+    def test_ALTERNATIVE(self):
+        p = PatternElement('a', 'b')
+        p.mark_ALTERNATIVE()
+        self.assertFalse(p.isCOMMIT())
+        self.assertFalse(p.isREPEAT())
+        self.assertTrue(p.isALTERNATIVE())
+
+    def test_all(self):
+        p = PatternElement('a', 'b')
+        p.mark_COMMIT()
+        p.mark_REPEAT()
+        p.mark_ALTERNATIVE()
+        self.assertTrue(p.isCOMMIT())
+        self.assertTrue(p.isREPEAT())
+        self.assertTrue(p.isALTERNATIVE())
+
+if __name__ == "__main__":
+    unittest.main()
