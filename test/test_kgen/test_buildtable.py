6abdb9491bf76f3a1f0246d02ac0fbc2f5e64ebe
Jagadeesh Babu <jagadeeshbabue@gmail.com>
2011-05-30 17:28:17 +0200

    add column test cases

--- test/test_kgen/test_buildtable.py
+++ test/test_kgen/test_buildtable.py
@@ -30,6 +30,38 @@ class buildtableTest(unittest.TestCase):
         self.assertEqual([PE('p','b',PE.COMMIT),PE('a'),PE('c')], result)
 
 
+    def test_only_rule_case2(self):
+        data = '''RULE 
+            p:b <= a c _
+        '''
+        mockAST = MockAST()
+        self.kparser.parse(data, mockAST)
+        result = build_kgen_table(mockAST.rules[0][2], mockAST.rules[0][3])
+        
+        self.assertEqual([PE('p','b'),PE('p','@'),PE('a'),PE('c')], result)
+
+
+    def test_only_rule_case3(self):
+        data = '''RULE 
+            p:b <=> a c _
+        '''
+        mockAST = MockAST()
+        self.kparser.parse(data, mockAST)
+        result = build_kgen_table(mockAST.rules[0][2], mockAST.rules[0][3])
+        
+        self.assertEqual([PE('p','b'),PE('p','@'),PE('p','b',PE.COMMIT),PE('a'),PE('c')], result)
+
+
+    def test_only_rule_case4(self):
+        data = '''RULE 
+            p:b /<= a c _
+        '''
+        mockAST = MockAST()
+        self.kparser.parse(data, mockAST)
+        result = build_kgen_table(mockAST.rules[0][2], mockAST.rules[0][3])
+        
+        self.assertEqual([PE('a'),PE('c'),PE('p','b')], result)
+
 
 if __name__ == "__main__":
     unittest.main()
