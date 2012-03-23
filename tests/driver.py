67dab9d1f404bceef34d10ac6113c40e8b0187a2
Jagadeesh Babu <jagadeeshbabue@gmail.com>
2012-03-23 09:30:38 +0100

    moved assertion to the DataRecord so that it can be overriden

--- tests/driver.py
+++ tests/driver.py
@@ -4,7 +4,6 @@ Created on Mar 15, 2012
 @author: jagadeesh
 '''
 import unittest
-from functools import update_wrapper
 
 class DataRecord:
     
@@ -15,40 +14,24 @@ class DataRecord:
         else:
             self.data = data
         self.expectation = expectation
-
-
-class DataDrivenTest:
     
-    def __init__(self, data_list):
-        self.data_list = data_list
-    
-    def __call__(self, func):
-        self.func = func
-        
-        def _driver(*args):
-            for record in self.data_list:
-                actual = self.func(*(args + record.data))
-                if not actual == record.expectation:
-                    raise AssertionError, \
-                        ('%s %r != %r' % (record.name, actual, record.expectation))
-        
-        update_wrapper(_driver, func)
-        return _driver
+    def verify(self, test, actual):
+        test.assertEqual(actual, self.expectation)
 
 
 def generate_class(class_name, func, data_list):
-    def test_wrapper(name, expectation, *args):
+    def test_wrapper(record):
         def test_wrapped(self):
-            actual = func(*args)
-            self.assertEqual(actual, expectation)
+            actual = func(*record.data)
+            record.verify(self, actual)
         
-        setattr(test_wrapped, '__doc__', 'test for %s' % name)
+        setattr(test_wrapped, '__doc__', 'test for %s' % record.name)
         return test_wrapped
 
     tests = {}
     index = 0
     for record in data_list:
-        testfunc = test_wrapper(record.name, record.expectation, *record.data)
+        testfunc = test_wrapper(record)
         tests['test_case_%s'%index] = testfunc
         index += 1
 
